f267b05efe9fecfe6cac7c3bf026a2786eb52a29
Bruno Axel Kamere <akamerebruno@yahoo.fr>
2020-12-13 03:10:52 +0100

    2:08:31

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store()
+    {
+        $data = request()->validate([
+            'caption' => 'required',
+            'image'=>['required', 'image'],
+        ]);
+
+        dd(\request(ima))
+
+        auth()->user()->posts()->Create($data);
+
+        dd(request()->all());
+    }
+}
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -7,15 +7,18 @@
             <img src="https://scontent-waw1-1.cdninstagram.com/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com&_nc_ohc=dzidqACw0CkAX_25YmO&tp=1&oh=7675a9539eff5ce9b4d6f930c8658d1a&oe=5FFCBD67" class="rounded-circle" alt="">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>freecodecamp</h1></div>
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username}}</h1>
+                <a href="#">Add New Post</a>
+            </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
                 <div class="pr-5"><strong>212</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
-            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
-            <div><a href="#">www.freecodecamp.org</a></div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href="#">{{ $user->profile->url ?? 'No Url Set'}}</a></div>
         </div>
     </div>
     <div class="row pt-5">
