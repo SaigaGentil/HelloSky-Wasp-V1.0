db8786009b23ea3b2f1306eab7fdacc08041cc93
Bruno Axel Kamere <akamerebruno@yahoo.fr>
2020-12-16 06:18:37 +0100

    Fixed web route issue for add new post

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,18 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
 */
 
 Route::get('/', function () {
@@ -26,10 +38,11 @@ Route::get('/phpinfo', function() {
 Auth::routes();
 
 //  Route::get('/p', 'PostsController::class');
-Route::get('/p/{post}', [\App\Http\Controllers\PostsController::class, 'show']);
 Route::get('/p/create', [\App\Http\Controllers\PostsController::class, 'create']);
+Route::get('/p/{post}', [\App\Http\Controllers\PostsController::class, 'show']);
 Route::post('/p', [\App\Http\Controllers\PostsController::class, 'store']);
 
 Route::get('/profile/{user}', [\App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
+Route::get('/profile/{user}/edit', [\App\Http\Controllers\ProfilesController::class, 'edit'])->name('profile.edit');
 
 
