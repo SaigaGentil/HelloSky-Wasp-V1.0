52e95d23f8de1d78e8f97c8767f086728684c17f
Bruno Axel Kamere <akamerebruno@yahoo.fr>
2021-02-03 17:08:24 +0100

    a few changes

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -14,6 +14,6 @@ class AdminController extends Controller
 
     public function showLoginHistory()
     {
-        return view('admin.login_history');
+        return view('Admin.login_history');
     }
 }
